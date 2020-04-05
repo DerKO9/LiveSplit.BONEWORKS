146ca799b14d345a84801896213e3587a6fc4bb6
DerKO <dr.knockout99@gmail.com>
2020-04-05 15:29:23 -0600

    Add settings

--- LiveSplit.BONEWORKS/BoneworksComponent.cs
+++ LiveSplit.BONEWORKS/BoneworksComponent.cs
@@ -10,11 +10,21 @@ using System.Xml;
 using LiveSplit.Model;
 using LiveSplit.UI;
 using LiveSplit.UI.Components;
+using System.Diagnostics;
+using SpeedrunComSharp;
+using System.Threading;
 
 namespace LiveSplit.BONEWORKS
 {
     public class BoneworksComponent : IComponent
     {
+
+        public BoneworksComponent(LiveSplitState state)
+        {
+            _settings = new BoneworksSettings();
+            _state = state;
+        }
+
         public string ComponentName => "BONEWORKS Autosplitter";
         public float HorizontalWidth => 0;
         public float MinimumHeight => 0;
@@ -25,16 +35,21 @@ namespace LiveSplit.BONEWORKS
         public float PaddingLeft => 0;
         public float PaddingRight => 0;
 
-        private BoneworksSettings _settings; 
+        private BoneworksSettings _settings;
+
+        private LiveSplitState _state;
 
         public IDictionary<string, Action> ContextMenuControls => throw new NotImplementedException();
 
         public void Dispose() { }
-        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion) { }
-        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion) { }
-        public XmlNode GetSettings(XmlDocument document) { return null; }
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, System.Drawing.Region clipRegion) { }
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, System.Drawing.Region clipRegion) { }
+        public XmlNode GetSettings(XmlDocument document) { return _settings.UpdateSettings(document); }
         public Control GetSettingsControl(LayoutMode mode) { return _settings; }
         public void SetSettings(XmlNode settings) { }
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode) { }
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode) 
+        {
+            
+        }
     }
 }
--- LiveSplit.BONEWORKS/BoneworksSettings.cs
+++ LiveSplit.BONEWORKS/BoneworksSettings.cs
@@ -1,8 +1,61 @@
 ﻿using System.Windows.Forms;
+using System.Xml;
 
 namespace LiveSplit.BONEWORKS
 {
     internal class BoneworksSettings : UserControl
     {
+        private CheckBox checkBox1;
+
+        public BoneworksSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // BoneworksSettings
+            // 
+            this.Controls.Add(this.checkBox1);
+            this.Name = "BoneworksSettings";
+            this.Load += new System.EventHandler(this.BoneworksSettings_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void BoneworksSettings_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public XmlNode UpdateSettings(XmlDocument document)
+        {
+            XmlElement xmlSettings = document.CreateElement("Settings");
+
+            AddXmlItem<bool>(document, xmlSettings, "Test", checkBox1.Checked);
+
+            return xmlSettings;
+        }
+
+        public void AddXmlItem<T>(XmlDocument document, XmlElement xmlSettings, string name, T value)
+        {
+            XmlElement xmlItem = document.CreateElement(name);
+            xmlItem.InnerText = value.ToString();
+            xmlSettings.AppendChild(xmlItem);
+        }
     }
 }
